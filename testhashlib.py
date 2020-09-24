c88e4ecc521b3e52a97094caf08d32fe06b5a3a1
amr2233 <68191695+amr2233@users.noreply.github.com>
2020-09-24 07:01:28 -0600

    Delete testhashlib.py

--- testhashlib.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-class hashtext:
-    def hashingtext(self,text,hash_type):
-        text = text.encode("utf-8")
-        hash_hash = hashlib.new(hash_type)
-        hash_hash.update(text)
-        return hash_hash.hexdigest()
